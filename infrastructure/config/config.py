0df7038a12bcf63fa88b031dce5c2ca583af1b0f
Nilson Junior <nilsonjrsilva97@gmail.com>
2026-08-06 22:30:07 -0300

    alterando secret

--- infrastructure/config/config.py
+++ infrastructure/config/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    SECRET_KEY: str = "uma-chave-super-secreta"
+    SECRET_KEY: str = "42B38697D6C921058DDCFDD5ED5D89FAF0C671E3"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
